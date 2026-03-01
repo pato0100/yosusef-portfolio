b9a601db7b4ad7f62fe8c3658795b83c11ea734b
Youssef Mahmoud <almasrygrop0100@gmail.com>
2026-03-01 19:53:57 +0200

    first commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,6 +5,7 @@ import { motion } from 'framer-motion'
 export default function Navbar() {
   const { t, lang } = useI18n()
   const { pathname } = useLocation()
+  const { slug } = useParams()
    
   const tabs = [
     { to: `/${slug}`, label: t.profile },
