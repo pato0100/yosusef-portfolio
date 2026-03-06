d1061813a0d8c2a6637618c0dfe8a143826289c4
Youssef Mahmoud <almasrygrop0100@gmail.com>
2026-03-06 21:00:55 +0200

    first commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,7 +11,7 @@ export default function Navbar({ settings }) {
 const currentSlug = pathParts[0]
 
 // الصفحات التي لا يظهر فيها Navbar
-const hiddenRoutes = ["forgot-password", "login", "signup", "reset-password"]
+const hiddenRoutes = ["forgot-password", "login", "admin", "reset-password"]
 
 if (!currentSlug || hiddenRoutes.includes(currentSlug)) return null
 
